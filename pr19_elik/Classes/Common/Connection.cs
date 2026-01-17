2aba8d85993d8c43105eef7343362c3b489bbf11
ilmasheva-eliza <ilmeliza36@gmai.com>
2026-01-17 22:53:15 +0500

    Создание класса для работы с бд и подключение.Ильмашева

---
+++ pr19_elik/Classes/Common/Connection.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr19_elik.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string config = "server=10.0.204.1;uid=root;pwd=;database=kino;";
+
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+
+            return connection;
+        }
+
+        public MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+
+        public void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
